7aa66da778196d95df58841c3830a93d37dfc34a
Martin Friedmann <friedmann_martin@outlook.com>
2020-03-15 21:00:47 +0100

    Oprava překlepů

--- tabulka.php
+++ tabulka.php
@@ -1,20 +1,24 @@
 <table>
     <thead>
         <tr>
-            <th><a href=".index.php?sort=jmeno">Jméno</a></th>
-            <th><a href=".index.php?sort=prijmeni">Příjmení</a></th>
-            <th><a href=".index.php?sort=pohlavi">Pohlaví</a></th>
-            <th><a href=".index.php?sort=ulice">Ulice</a></th>
-            <th><a href=".index.php?sort=obec">Obec</a></th>
-            <th><a href=".index.php?sort=psc">PSČ</a></th>
-            <th><a href=".index.php?sort=telefon">Telefon</a></th>
-            <th><a href=".index.php?sort=email">E-mail</a></th>
-            <th><a href=".index.php?sort=pozice">Pozice</a></th>
-            <th><a href=".index.php?sort=nadrizeny">Nadřízený</a></th>
+            <th><a href="./index.php?sort=jmeno">Jméno</a></th>
+            <th><a href="./index.php?sort=prijmeni">Příjmení</a></th>
+            <th><a href="./index.php?sort=pohlavi">Pohlaví</a></th>
+            <th><a href="./index.php?sort=ulice">Ulice</a></th>
+            <th><a href="./index.php?sort=obec">Obec</a></th>
+            <th><a href="./index.php?sort=psc">PSČ</a></th>
+            <th><a href="./index.php?sort=telefon">Telefon</a></th>
+            <th><a href="./index.php?sort=email">E-mail</a></th>
+            <th><a href="./index.php?sort=pozice">Pozice</a></th>
+            <th><a href="./index.php?sort=nadrizeny">Nadřízený</a></th>
         </tr>
     </thead>
     <tbody>
         <?php
+            if(isset($_GET['sort'])){
+                usort($lide,function ($a,$b) {return strcmp($a->$_GET['sort'], $b->$_GET['sort']);});
+            }
+
             foreach ($lide as $id => $radek) {
                 if ($id == 0) continue;
                 echo '<tr>';
