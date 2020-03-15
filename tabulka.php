fd1f5a631d8637b143b3c2a029a6565215756317
Martin Friedmann <friedmann_martin@outlook.com>
2020-03-15 20:07:36 +0100

    Refaktorizace a zjednodušení kódu

--- tabulka.php
+++ tabulka.php
@@ -1,21 +1,26 @@
 <table>
     <thead>
         <tr>
-            <?php
-                foreach ($lide['zahlavi'] as $sloupec) {
-                    echo "<th>$sloupec</th>";
-                }
-            ?>
+            <th>Jméno</th>
+            <th>Příjmení</th>
+            <th>Pohlaví</th>
+            <th>Ulice</th>
+            <th>Obec</th>
+            <th>PSČ</th>
+            <th>Telefon</th>
+            <th>E-mail</th>
+            <th>Pozice</th>
+            <th>Nadřízený</th>
         </tr>
     </thead>
     <tbody>
         <?php
-            foreach (array_slice($lide, 1) as $id => $radek) {
+            foreach ($lide as $id => $radek) {
+                if ($id == 0) continue;
                 echo '<tr>';
                 foreach ($radek as $sloupec) {
                     echo "<td>$sloupec</td>";
                 }
-                $id++;
                 echo "<td><a href='./index.php?id=$id'>⚙️</a></td>";
                 echo "</tr>\n";
             }
