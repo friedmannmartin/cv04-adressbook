c1ee7d45f5629cb0067cb9ead5c160a3268fbd64
Martin Friedmann <friedmann_martin@outlook.com>
2020-03-15 18:18:51 +0100

    Změna construktoru Osoby

--- index.php
+++ index.php
@@ -12,7 +12,17 @@
         <?php
             $soubor = fopen("adresar.csv","r+");
             while(($radek = fgetcsv($soubor,0,';')) !== FALSE){
-                $_lide[] = new Osoba($radek[0],$radek[1],$radek[2],$radek[3],$radek[4],$radek[5],$radek[6],$radek[7],$radek[8],$radek[9]);
+                $_lide[] = new Osoba();
+                end($_lide)->jmeno = $radek[0];
+                end($_lide)->prijmeni = $radek[1];
+                end($_lide)->pohlavi = $radek[2];
+                end($_lide)->ulice = $radek[3];
+                end($_lide)->obec = $radek[4];
+                end($_lide)->psc = $radek[5];
+                end($_lide)->telefon = $radek[6];
+                end($_lide)->email = $radek[7];
+                end($_lide)->pozice = $radek[8];
+                end($_lide)->nadrizeny = $radek[9];
             }
             fclose($soubor);
 
--- osoba.php
+++ osoba.php
@@ -11,19 +11,6 @@ class Osoba {
     public $pozice;
     public $nadrizeny;
 
-    public function __construct($jmeno,$prijmeni,$pohlavi,$ulice,$obec,$psc,$telefon,$email,$pozice,$nadrizeny){
-        $this->jmeno     = $jmeno;
-        $this->prijmeni  = $prijmeni;
-        $this->pohlavi   = $pohlavi;
-        $this->ulice     = $ulice;
-        $this->obec      = $obec;
-        $this->psc       = $psc;
-        $this->telefon   = $telefon;
-        $this->email     = $email;
-        $this->pozice    = $pozice;
-        $this->nadrizeny = $nadrizeny;
-    }
-
     public function __toString() {
         return $this->jmeno . ' ' . $this->prijmeni;
     }
