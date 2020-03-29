25d46bc6e9fc15880d899591a84062b7ac22aad2
Martin Friedmann <friedmann_martin@outlook.com>
2020-03-29 22:15:50 +0200

    Další změny pro zobrazení na nižším rozlišení

--- tabulka.php
+++ tabulka.php
@@ -1,40 +1,42 @@
-<table>
-    <thead>
-        <tr>
-            <th><a href="./index.php?sort=jmeno">Jméno</a></th>
-            <th><a href="./index.php?sort=prijmeni">Příjmení</a></th>
-            <th><a href="./index.php?sort=pohlavi">Pohlaví</a></th>
-            <th><a href="./index.php?sort=ulice">Ulice</a></th>
-            <th><a href="./index.php?sort=obec">Obec</a></th>
-            <th><a href="./index.php?sort=psc">PSČ</a></th>
-            <th><a href="./index.php?sort=telefon">Telefon</a></th>
-            <th><a href="./index.php?sort=email">E-mail</a></th>
-            <th><a href="./index.php?sort=pozice">Pozice</a></th>
-            <th><a href="./index.php?sort=nadrizeny">Nadřízený</a></th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th><a href="./index.php?sort=jmeno">Jméno</a></th>
+                <th><a href="./index.php?sort=prijmeni">Příjmení</a></th>
+                <th><a href="./index.php?sort=pohlavi">Pohlaví</a></th>
+                <th><a href="./index.php?sort=ulice">Ulice</a></th>
+                <th><a href="./index.php?sort=obec">Obec</a></th>
+                <th><a href="./index.php?sort=psc">PSČ</a></th>
+                <th><a href="./index.php?sort=telefon">Telefon</a></th>
+                <th><a href="./index.php?sort=email">E-mail</a></th>
+                <th><a href="./index.php?sort=pozice">Pozice</a></th>
+                <th><a href="./index.php?sort=nadrizeny">Nadřízený</a></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
 
-            //Nastavení řazení
-            if(isset($_GET['sort'])){
-                usort($lideBezZahlavi, function($a, $b){
-                    $param = $_GET['sort'];
-                    return strcmp($a->$param, $b->$param);
-                });
-            }
-
-            //výpis jednotlivých uživatelů po řádcích
-            foreach ($lideBezZahlavi as $radek) {
-                echo '<tr>';
-                foreach ($radek as $sloupec) {
-                    echo "<td>$sloupec</td>";
+                //Nastavení řazení
+                if(isset($_GET['sort'])){
+                    usort($lideBezZahlavi, function($a, $b){
+                        $param = $_GET['sort'];
+                        return strcmp($a->$param, $b->$param);
+                    });
                 }
-                echo "<td><a href='./index.php?osoba=$radek'>🛠️</a></td>";
-                echo "</tr>\n";
-            }
 
-        ?>
-    </tbody>
-</table>
+                //výpis jednotlivých uživatelů po řádcích
+                foreach ($lideBezZahlavi as $radek) {
+                    echo '<tr>';
+                    foreach ($radek as $sloupec) {
+                        echo "<td>$sloupec</td>";
+                    }
+                    echo "<td><a href='./index.php?osoba=$radek'>🛠️</a></td>";
+                    echo "</tr>\n";
+                }
+
+            ?>
+        </tbody>
+    </table>
+</div>
